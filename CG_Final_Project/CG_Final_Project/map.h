4afcd8bd6a4c45bab341efb959fcd49e43af4a73
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-12-15 01:48:58 +0900

    맵완성
    ㄹㅇㅋㅋ

--- CG_Final_Project/CG_Final_Project/map.h
+++ CG_Final_Project/CG_Final_Project/map.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "global.h"
 void draw_floor(GLuint ShaderProgram, GLuint VBO, GLuint EBO);
-void draw_wall(GLuint ShaderProgram, GLuint VBO, GLuint EBO, float x, float z,float width);
+void draw_wall(GLuint ShaderProgram, GLuint VBO, GLuint EBO, float x, float z, float width, float width2);
 
 void draw_map(GLuint ShaderProgram, GLuint VBO, GLuint EBO);
