9bbbfd8c253153133c6b57be1c1f4b6ad997284e
SuperHong94 <50072558+SuperHong94@users.noreply.github.com>
2019-12-16 01:20:33 +0900

    jj
    jj

---
+++ CG_Final_Project/CG_Final_Project/main.cpp
@@ -0,0 +1,325 @@
+<<<<<<< HEAD
+=======
+#include <Windows.h>
+#include "InitApp.h"
+#include "Pacman.h"
+
+#define WINDOW_POSITION 100		// ¿©µµøÏ∞° Ω∫≈©∏∞¿« æÓµø°º≠ Ω√¿€«œ¥¬¡ˆ -> Ω∫≈©∏∞ ¡¬ªÛ¥‹ ±‚¡ÿ x,x ø°º≠ ¿©µµøÏ∞° ø≠∏≤
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "freeglut.lib")
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w, int h);
+GLuint ShaderProgram;
+GLuint ConVBO;
+GLuint ConEBO;
+GLuint VBO;
+GLuint EBO;
+
+
+GLfloat xAngle = 0.0f, yAngle = 0.0f;
+GLfloat tempx = 0.0f, tempy = 0.0f;		// ¿Ã¿¸¿« ∏∂øÏΩ∫ ∞™
+
+glm::vec3 EYE = glm::vec3(0.0f, 1.0f, 0.5f);
+glm::vec3 AT = glm::vec3(-1.0f, 1.0f, 0.5f);
+glm::vec3 UP = glm::vec3(0.0f, 1.0f, 0.0f);
+
+glm::vec3 T_EYE = glm::vec3(0.0f, 20.0f, 0.01f);		// ≈æ∫‰±‚¡ÿ ƒ´∏ﬁ∂Û EYE
+glm::vec3 T_AT = glm::vec3(0.0f, 0.0f, 0.0f);		// ≈æ∫‰±‚¡ÿ ƒ´∏ﬁ∂Û AT
+
+bool Left = false, Right = false, Up = false, Down = false;		// ≈∞ ¿‘∑¬
+bool move = false;
+bool click = false;
+Pacman *pacman = new Pacman;
+
+EViewPoint view_point = E_DEFAULT_VIEW;
+
+void Mouse(int button, int state, int x, int y);
+void MouseMotion(int x, int y);
+void Timer(int a);
+void InputKey(unsigned char key, int x, int y);
+void KeyUP(unsigned char key, int x, int y);
+bool check_move();								// ¿Ãµø≈∞∞° ¥≠∑»≥™ »Æ¿Œ«œ¥¬ «‘ºˆ
+void Set_Cursor();
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(WINDOW_POSITION, WINDOW_POSITION);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutCreateWindow("Example1");
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
+	glEnable(GL_CULL_FACE);
+	CreateCon(ConEBO, ConVBO);
+	CreateCube(ShaderProgram, EBO, VBO);
+
+	Set_Cursor();				// ƒøº≠ Ω√¿€¡ˆ¡° º≥¡§
+	init_wall();				// ∫Æ ¡¬«• º≥¡§
+
+	InitProgram(ShaderProgram);
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(InputKey);			// ≈∞∫∏µÂ ¿‘∑¬
+	glutKeyboardUpFunc(KeyUP);			// ≈∞∫∏µÂ ∂º¥¬ ∞Õ
+	glutMouseFunc(Mouse);				// ∏∂øÏΩ∫ ≈¨∏Ø
+	glutMotionFunc(MouseMotion);
+	glutTimerFunc(10, Timer, 1);
+	glutReshapeFunc(Reshape);
+	glutMainLoop();
+}
+
+void Timer(int a)
+{
+	if (move) {
+		pacman->Move(Up, Down, Right, Left, EYE, AT, UP);
+
+
+	}
+
+
+	glutPostRedisplay();
+
+	if(move)			// øÚ¡˜¿Ã¥¬ ªÛ≈¬∏È ≈∏¿Ã∏” ¿Á»£√‚
+		glutTimerFunc(10, Timer, a);
+}
+
+GLvoid drawScene()
+{
+	//
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	glUseProgram(ShaderProgram);
+
+
+	view(ShaderProgram, view_point, EYE, AT, UP, T_EYE, T_AT);	// ƒ´∏ﬁ∂Û 
+
+	Myprojection(ShaderProgram, view_point);
+
+	draw_map(ShaderProgram, VBO, EBO);
+
+	//¿”Ω√ «√∑π¿ÃæÓ ¿ßƒ°
+	//draw_wall(ShaderProgram, VBO, EBO, EYE.x, EYE.z, 0.5f, 0.5f);		// ƒ´∏ﬁ∂Û¿ßƒ° = ƒ≥∏Ø≈Õ¿ßƒ° -> 1¿Œƒ™
+	
+	//¿”Ω√ «√∑π¿ÃæÓ ¿ßƒ°
+	pacman->Draw(ShaderProgram, VBO, EBO);
+
+
+	float r = 3.0f;
+
+
+	glutSwapBuffers();
+
+
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+
+}
+
+void InputKey(unsigned char key, int x, int y)
+{
+	static bool culling = false;
+	switch (key)
+	{
+	case '1':
+		view_point = E_DEFAULT_VIEW;
+		break;
+	case '3':
+		view_point = E_TOP_VIEW;
+		break;
+	case 'y':
+
+		break;
+
+	case 'w':
+		Up = true;
+			
+		if (!move) {				// ¿Ãµø¡ﬂ¿Ã∏È timer «‘ºˆ∏¶ »£√‚ X
+			move = true;
+			glutTimerFunc(10, Timer, 1);
+		}
+
+		break;
+
+	case 'a':
+		Left = true;
+
+		if (!move) {				// ¿Ãµø¡ﬂ¿Ã∏È timer «‘ºˆ∏¶ »£√‚ X
+			move = true;
+			glutTimerFunc(10, Timer, 1);
+		}
+
+		break;
+
+	case 's':
+		Down = true;
+
+		if (!move) {				// ¿Ãµø¡ﬂ¿Ã∏È timer «‘ºˆ∏¶ »£√‚ X
+			move = true;
+			glutTimerFunc(10, Timer, 1);
+		}
+
+		break;
+
+	case 'd':
+		Right = true;
+
+		if (!move) {				// ¿Ãµø¡ﬂ¿Ã∏È timer «‘ºˆ∏¶ »£√‚ X
+			move = true;
+			glutTimerFunc(10, Timer, 1);
+		}
+
+		break;
+
+	case 'z':
+		break;
+	case 'x':
+		break;
+
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+
+	default:
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+void KeyUP(unsigned char key, int x, int y)
+{
+	switch (key){
+	case 'w':
+		Up = false;
+
+		if (!check_move()) {		// ¿Ãµø«œ¡ˆ æ ¿∏∏È move «‘ºˆ∏¶ false∑Œ ∏∏µÎ
+			move = false;
+		}
+
+		break;
+
+	case 'a':
+		Left = false;
+
+		if (!check_move()) {
+			move = false;
+		}
+
+		break;
+
+	case 's':
+		Down = false;
+
+		if (!check_move()) {
+			move = false;
+		}
+
+		break;
+
+	case 'd':
+		Right = false;
+
+		if (!check_move()) {
+			move = false;
+		}
+
+		break;
+
+	}
+
+	glutPostRedisplay();
+}
+
+void Mouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		click = true;
+	}
+	else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		click = false;
+		Set_Cursor();
+	}
+	glutPostRedisplay();
+}
+
+void MouseMotion(int x, int y)
+{
+	if (click) {
+		int width = WINDOW_WIDTH / 2;
+		int height = WINDOW_HEIGHT / 2;
+
+		GLfloat mx = (GLfloat)(x - width) / (GLfloat)width;			// ∏∂øÏΩ∫ ¿Ãµø«— ¡¬«•
+		GLfloat my = (GLfloat)(height - y) / (GLfloat)height;
+
+
+		// «ˆ¿Á «¡∑π¿”∞˙ ¿Ã¿¸«¡∑π¿”¿« ∏∂øÏΩ∫ ¡¬«• ¬˜¿Ã
+		xAngle += (my - tempy) * 3.6;				// ¿ßæ∆∑° ¿Ãµø Ω√ x√‡ ±‚¡ÿ »∏¿¸		µ⁄¿« º˝¿⁄¥¬ πË¿≤
+		yAngle += (mx - tempx) / 2.0;				// ¡¬øÏ ¿ÃµøΩ√ y√‡ ±‚¡ÿ »∏¿¸
+
+
+		std::cout << mx << " " << my << " " << tempx << " " << tempy << std::endl;
+
+
+
+		glm::mat4 r = glm::mat4(1.0f);		// »∏¿¸
+
+		r = glm::rotate(r, glm::radians(1.0f), glm::vec3(xAngle, yAngle, 0.0f));
+
+		glm::mat3 rm = glm::mat3(r);		// ¿ß¿« »∏¿¸«‡∑ƒø°º≠ 3x3 ∫Œ∫–∏∏ µ˚ø»
+
+
+		// AT ¿« EYE ±‚¡ÿ »∏¿¸
+		AT = AT - EYE;		// EYE∏¶ ø¯¡°¿Ãµø
+		AT = AT * rm;		// AT¿ª »∏¿¸
+		AT = AT + EYE;		// ¿Ãµø«ﬂ¥¯ ∏∏≈≠ ¥ŸΩ√ ø™¿Ãµø
+
+		AT.y += xAngle;
+
+		xAngle = 0.0f;
+		yAngle = 0.0f;
+
+		tempx = mx;
+		tempy = my;
+
+		glutPostRedisplay();
+	}
+}
+
+bool check_move()		// ¿Ãµø≈∞∞° ¥≠∑¡¿÷¿∏∏È true, æ∆¥œ∏È false
+{
+	if (Left || Right || Up || Down)
+		return true;
+	else
+		return false;
+}
+
+void Set_Cursor()
+{
+	int width = WINDOW_WIDTH / 2;
+	int height = WINDOW_HEIGHT / 2;
+
+	int x = WINDOW_WIDTH / 2 + WINDOW_POSITION + 11;
+	int y = WINDOW_HEIGHT / 2 + WINDOW_POSITION + 31;
+
+	SetCursorPos(x, y);		// Ω√¿€ ∏∂øÏΩ∫ ƒøº≠ ¿ßƒ° º≥¡§
+
+
+	//tempx = (GLfloat)(x - width) / width;			// Ω√¿€ ∏∂øÏΩ∫ ¿ßƒ°¿« openGL x¡¬«•∏¶ πﬁ¿Ω
+	//tempy = (GLfloat)(height - y) / height;		// Ω√¿€ ∏∂øÏΩ∫ ¿ßƒ°¿« openGL y¡¬«•∏¶ πﬁ¿Ω
+
+	tempx = 0.0f;
+	tempy = 0.0f;
+}
+>>>>>>> parent of 7e56ed6... Ïπ¥Î©îÎùº Íµ¨ÌòÑ
